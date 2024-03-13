075c7447293e51f30825215467090e66ca770d31
UmeshDesai123 <umeshdesai1995@gmail.com>
2024-03-14 00:37:11 +0530

    2nd solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,49 @@
 
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import './../styles/App.css';
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = { name: null};
-  }
+// class App extends Component{
+//   constructor(props){
+//     super(props)
+//     this.state = { name: null};
+//   }
   
-  updateText = ()=>{
-    this.setState({
-      name: document.getElementById("name").value
-    });
+//   updateText = ()=>{
+//     this.setState({
+//       name: document.getElementById("name").value
+//     });
+//   }
+//   render(){
+//     return (
+//       <div>
+//           {/* Do not remove the main div */}
+//           <p>Enter your name:</p>
+//           <input id="name" type="text" onChange={this.updateText}></input>
+//           {
+//             this.state.name ? <p>Hello {this.state.name}!</p> : null
+//           }
+//       </div>
+//     )
+//   }
+// }
+
+const App = ()=>{
+  const [state, setState ] = useState("");
+
+  const updateName = (event)=>{
+    
+    setState(event.target.value)
   }
-  render(){
-    return (
-      <div>
-          {/* Do not remove the main div */}
-          <p>Enter your name:</p>
-          <input id="name" type="text" onChange={this.updateText}></input>
-          {
-            this.state.name ? <p>Hello {this.state.name}!</p> : null
-          }
-      </div>
-    )
-  }
+
+  return(
+    <div>
+      <p>Enter your name:</p>
+      <input id="name" type="text" onChange={updateName}></input>
+      {
+        state && <p>Hello {state}!</p>
+      }
+    </div>
+  )
 }
 
 export default App
