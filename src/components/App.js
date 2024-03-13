6836bd070e1cd57ff544bb32de7ca732bbd13b7e
UmeshDesai123 <umeshdesai1995@gmail.com>
2024-03-13 10:43:56 +0530

    1st solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 
-import React from "react";
+import React, { Component } from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = { name: null};
+  }
+  
+  updateText = ()=>{
+    this.setState({
+      name: document.getElementById("name").value
+    });
+  }
+  render(){
+    return (
+      <div>
+          {/* Do not remove the main div */}
+          <p>Enter your name:</p>
+          <input id="name" type="text" onChange={this.updateText}></input>
+          {
+            this.state.name ? <p>Hello {this.state.name}!</p> : null
+          }
+      </div>
+    )
+  }
 }
 
 export default App
